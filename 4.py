78e9ac9ab20e30e2caa1c170f459edf07f97c0d2
Wntd73 <127610521+Wntd73@users.noreply.github.com>
2024-12-20 09:41:02 +0300

    4.py

--- 4.py
+++ 4.py
@@ -22,7 +22,7 @@ while s!=[]:
     for i in s:
         match = re.match(r"^-?[01234567]+$", i)
         if match and int(i,8)<=4096:
-            if ((len(i)%2==0 and '-' not in i) or ((len(i)-1)%2==0 and '-' in i)) and int(i)%2==0:
+            if ((len(i)%2==0 and '-' not in i) or ((len(i)-1)%2==0 and '-' in i)) and int(i)%2!=0:
                 for sym in i:
                     if sym!='-': print(d[sym], end = ' ')
                 print()
